c2e20fb5ad5adf9016996132039c78b6970e566b
amacore <amarock@tut.by>
2017-04-23 17:26:29 +0300

    Init

---
+++ CSKernel/Message.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace CSKernel
+{
+    public class Message
+    {
+        public List<string> Identity { get; set; }
+        public string Delimiter { get; set; }
+        public string HMacSignature { get; set; }
+        public Header Header { get; set; }
+        public Header ParentHeader { get; set; }
+        public Dictionary<string, object> Metadata { get; set; }
+        public string Content { get; set; }
+    }
+
+    public class Header
+    {
+        [JsonProperty("msg_id")]
+        public string MessageId { get; set; }
+        [JsonProperty("username")]
+        public string Username { get; set; }
+        [JsonProperty("session")]
+        public string Session { get; set; }
+        [JsonProperty("msg_type")]
+        public string MessageType { get; set; }
+        [JsonProperty("version")]
+        public string Version { get; set; }
+    }
+}
---
+++ CSKernel/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using NetMQ;
+using NetMQ.Sockets;
+using Newtonsoft.Json;
+
+namespace CSKernel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = JsonConvert.DeserializeObject<ConnectionFile>(File.ReadAllText(args[0]));
+
+            var source = new CancellationTokenSource();
+
+            var hbTask = new Task(() => ListenHeartbeat(file, source.Token));
+            hbTask.Start();
+
+            var ioEndpoint = $"{file.Transport}://{file.IP}:{file.IOPubPort}";
+            var shellEndpoint = $"{file.Transport}://{file.IP}:{file.ShellPort}";
+
+
+        }
+
+        private static void ListenHeartbeat(ConnectionFile file, CancellationToken ct)
+        {
+            var heatbeatEndpoint = $"{file.Transport}://{file.IP}:{file.HBPort}";
+            using (var responseSocket = new ResponseSocket())
+            {
+                responseSocket.Bind(heatbeatEndpoint);
+                while (!ct.IsCancellationRequested)
+                {
+                    var message = responseSocket.ReceiveFrameString();
+                    responseSocket.SendFrame(message);
+                }
+            }
+        }
+    }
+}
